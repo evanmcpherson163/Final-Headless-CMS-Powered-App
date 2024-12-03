7c322f93e4af2873b53f8a19de8946e71d636933
Evan McPherson <evanmcpherson98@yahoo.com>
2024-12-02 19:09:18 -0800

    initial commit

---
+++ Basic-Headless-CMS-main/basic-full-stack-app/library/data.js
@@ -0,0 +1,110 @@
+// use node default modules for fs and path
+//import fs from 'fs';
+//import path from 'path';
+
+import got from 'got';
+
+// const dataDirectory = path.join( process.cwd(), 'data' );
+
+const dataURL = "https://dev-headless-cms-powered-app.pantheonsite.io/wp-json/twentytwentyone-child/v1/special";
+
+
+
+export async function getSortedList(){
+    //get filepath to json file
+    //const filePath = path.join(dataDirectory , 'data1.json');
+    //load jsong file contents
+    //const jsonString = fs.readFileSync(filePath , 'utf8');
+    let jsonString;
+    try{
+        //console.log(dataURL);
+        jsonString = await got(dataURL);
+        //console.log(jsonString);
+    } catch(error){
+        jsonString = {body: ""};
+        console.log(error);
+    }
+    //convert string from file into json array object
+    //const jsonObj = JSON.parse(jsonString);
+    const jsonObj = JSON.parse(jsonString.body);
+
+    //sort json array by name property
+    jsonObj.sort(
+        function(a,b){
+            return a.post_title.localeCompare(b.name);
+        }
+    );
+    //use map() on array to extract just id + name properties into new array of obj values
+    return jsonObj.map(
+        function(item){
+            return{
+                id: item.ID.toString(),
+                name: item.post_title
+            };
+        }
+    );
+}
+
+export async function getAllIds(){
+    //get filepath to json file
+    //const filePath = path.join(dataDirectory , 'data1.json');
+    //load jsong file contents
+    //const jsonString = fs.readFileSync(filePath , 'utf8');
+    let jsonString;
+    try{
+        jsonString = await got(dataURL);
+        //console.log(jsonString.body);
+    } catch(error){
+        jsonString.post_content = [];
+        console.log(error);
+    }
+    //convert string from file into json array object
+    //const jsonObj = JSON.parse(jsonString);
+    const jsonObj = JSON.parse(jsonString.body);
+    //use map() on array to extract just id + name properties into new array of obj values
+    return jsonObj.map(
+        function(item){
+            return{
+                params: {
+                id: item.ID.toString(),
+                }
+            };
+        }
+    );
+}
+
+// function return ALL of the preperty for one single object with a matching id property value
+export async function getData(idRequested){
+    //get filepath to json file
+    //const filePath = path.join(dataDirectory , 'data1.json');
+    //load jsong file contents
+    //const jsonString = fs.readFileSync(filePath , 'utf8');
+    let jsonString;
+    try{
+        jsonString = await got(dataURL);
+        //console.log(jsonString.body);
+    } catch(error){
+        jsonString.body = [];
+        console.log(error);
+    }
+    //convert string from file into json array object
+    //const jsonObj = JSON.parse(jsonString);
+    const jsonObj = JSON.parse(jsonString.body);
+    // find object value in array that has matching id
+    const objMatch = jsonObj.filter(
+        function(obj) {
+            return obj.ID.toString() === idRequested;
+        }
+    );
+
+    // extract object value in filtered array if any 
+    let objReturned;
+    if (objMatch.length > 0) {
+        objReturned = objMatch[0];
+    } else{
+        objReturned = {};
+    }
+
+    //return object value found
+    return objReturned;
+}
---
+++ Basic-Headless-CMS-main/basic-full-stack-app/pages/[id].js
@@ -0,0 +1,36 @@
+// import Layout
+import Layout from '/components/layout';
+import { getAllIds, getData} from "/library/data";
+// any file name enclosed in [] expects to see both getStaticProps() and getStaticPaths() named exactly as shown
+// define a getStaticProps() function to have next.js retreive data to use for the dynamic page - this name is defined by next.js
+export async function getStaticProps( { params } ){
+    const itemData = await getData(params.id);
+    return {
+        props: {
+            itemData
+        }
+    };
+}
+// define a getStaticPaths() function to tell next.js all valid URLs- this name is defined by next.js
+export async function getStaticPaths() {
+    const paths = await getAllIds();
+    return {
+        paths,
+        fallback: false
+    };
+}
+// export our dynamically routed page component Entry
+// in JSX "class" must be changed to "className"
+export default function Entry( { itemData } ) {
+    return(
+        <Layout>
+            <article className="card col-6"> 
+                <div className="card-body">
+                    <h5 className="card-title">{itemData.post_title}</h5>
+                    <h6 className="card-text">{itemData.user_login}</h6>
+                    <div className="card-text" dangerouslySetInnerHTML={{__html: itemData.post_content}} />
+                </div>
+            </article>  
+        </Layout>
+    );
+}
